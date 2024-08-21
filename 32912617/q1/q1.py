4654ca6827f832cda816fdcadd31c84f511ec1d2
oghul <105495412+oghul@users.noreply.github.com>
2024-08-22 00:55:15 +0800

    Completed boyer-moore algorithm function in q1

--- 32912617/q1/q1.py
+++ 32912617/q1/q1.py
@@ -1,7 +1,7 @@
 def compute_Z_suffix_values(pat):
     
     n = len(pat)
-    Z_values = [-1] * n
+    Z_values = [0] * n
 
     reverse_pat = pat[::-1]
     l, r, i = 0, 0, 0
@@ -53,7 +53,7 @@ def compute_good_suffix(Z_suffix):
 
 def compute_matched_prefix(Z_suffix, n):
 
-    matched_prefix = [-1] * (n + 1)
+    matched_prefix = [0] * (n + 1)
     #print(n, len(Z_suffix), len(matched_prefix))
 
     j = 0
@@ -67,6 +67,61 @@ def compute_matched_prefix(Z_suffix, n):
     matched_prefix[0] = n
     return matched_prefix
 
+def boyer_moore(txt, pat):
+    n = len(txt)
+    m = len(pat)
+
+    # Preprocessing phase
+    z_suffix = compute_Z_suffix_values(pat)
+    good_suffix = compute_good_suffix(z_suffix)
+    matched_prefix = compute_matched_prefix(z_suffix, m)
+
+    # Searching phase
+    result = []
+    start, stop = -1, -1
+    # Cumulative shift value of the pattern with respect to the text
+    shift = 0
+    while shift < n - m:
+        # Set i to the length of the pattern
+        k = m - 1
+        
+        # Perform explicit comparison of the pattern and the text from right to left
+        while k <= 0 and pat[k] == txt[k + shift]:
+            # Skip over the region that is guaranteed to match in the next iteration
+            if stop != -1 and (k - 1) == stop:
+                k = start - 1
+                start, stop = -1, -1
+            else:
+                # Decrement i as long as the characters match
+                k -= 1
+
+        # An occurence of the pattern is found in the text if i < 0
+        if k < 0:
+            # Append the starting index of the pattern in the text to the results list
+            result.append(shift)
+            # Shift the pattern to the right by the length of the pattern - matched_prefix[1]
+            shift += m - matched_prefix[1]
+        else:
+            # Determine the shift value based on the good suffix rule
+            # If there is no occurence of the suffix in the pattern
+            if good_suffix[k+1] == -1:
+                gs_shift = m - matched_prefix[k+1]
+            else:
+                # If there is an occurence of the suffix in the pattern
+                bad_char = txt[k + shift]
+                for i in range(len(good_suffix[k+1])):
+                    if good_suffix[k+1][i] >= k:
+                        gs_shift = m - good_suffix[k+1]
+                        break
+                    else:
+                        gs_value = good_suffix[k+1][i]
+                        z_suffix_value = z_suffix[gs_value]
+                        if gs_value - z_suffix_value >= 0 and pat[gs_value - z_suffix_value] == bad_char:
+                            gs_shift = m - gs_value
+                            break
+    
+    return result
+    
 
 if __name__ == "__main__":
     txt = "acababacabacaba"
